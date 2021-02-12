6516577261696ec881f795ba2b8b727546a43708
edward-delossantos <35975985+edward-delossantos@users.noreply.github.com>
2021-02-12 01:16:54 -0500

    Full app

---
+++ flixter/MovieViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MovieViewCell.swift
+//  flixter
+//
+//  Created by Edward de los Santos on 2/12/21.
+//
+
+import UIKit
+
+class MovieViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
